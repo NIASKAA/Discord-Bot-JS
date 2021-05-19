fde001e81ad31c915dd280f1645449cefbe5f44f
NIASKA <mkman751@yahoo.com>
2021-05-19 08:55:49 -0500

    added token and some basic func

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ require('dotenv').config();
 const TOKEN  = process.env.TOKEN;
 
 bot.login(TOKEN);
+
+bot.on('ready', () => {
+    console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', msg => {
+    if(msg.content === 'ping') {
+        msg.reply('pong');
+        msg.channel.send('pong')
+    }
+});
