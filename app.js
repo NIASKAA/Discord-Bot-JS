5ad19d6b9a723cbcadb1fe10621968a21804fed5
NIASKA <mkman751@yahoo.com>
2021-05-19 20:11:44 -0500

    added more functionality

--- app.js
+++ app.js
@@ -16,3 +16,31 @@ bot.on('message', msg => {
         msg.channel.send('pong')
     }
 });
+
+bot.on('message', msg => {
+    if(msg.content === 'Hi') {
+        msg.reply('Harro')
+    }
+});
+
+bot.on('message', msg => {
+    if(msg.content === 'Hello') {
+        msg.reply('Harro')
+    }
+});
+
+const badjokes = 
+[
+    "Why did the chicken cross the road? Because it was disable",
+    "I want to fucking kill myself.. That's funny right?",
+    "IT'S THAT A SUPRA? ",
+    "Is that a transformer? ",
+    "WHAT DO YOU MEANNNN THO?",
+    "Stop. Get some help."
+];
+
+bot.on('message', (msg) => {
+    if(msg.content === '?joke') {
+        msg.channel.send(badjokes[Math.floor(Math.random() * badjokes.length)]);
+    }
+});
