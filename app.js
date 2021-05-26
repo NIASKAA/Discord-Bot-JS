3e6b154073eeb6c95c427425201e785982f19ae2
NIASKA <mkman751@yahoo.com>
2021-05-26 10:15:41 -0500

    small addons

--- app.js
+++ app.js
@@ -28,14 +28,26 @@ bot.on('message', msg => {
     }
 });
 
+bot.on('message', msg => {
+    if(msg.content === 'Marco') {
+        msg.channel.send('Pollo bitch')
+    }
+});
+
+
+
 const badjokes = 
 [
-    "Why did the chicken cross the road? Because it was disable",
+    "Why did the chicken cross the road? Because it was disabled",
     "I want to fucking kill myself.. That's funny right?",
-    "IT'S THAT A SUPRA? ",
-    "Is that a transformer? ",
+    "IS THAT A SUPRA?",
+    "Is that a transformer?",
     "WHAT DO YOU MEANNNN THO?",
-    "Stop. Get some help."
+    "Stop. Get some help.",
+    "Error. System reboot. Sike, fuck you.",
+    "What do you call an alligator that wants to be a detective? An investigator. ACK ACK ACK",
+    "HAMBURGER PLS",
+
 ];
 
 bot.on('message', (msg) => {
