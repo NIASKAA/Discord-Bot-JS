71451e58a912932f31b63c03d9b21da7b40054f0
NIASKA <mkman751@yahoo.com>
2021-06-30 23:44:34 -0500

    changed shit

--- app.js
+++ app.js
@@ -29,17 +29,3 @@ client.on('guildMemberAdd', guildMember => {
     let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'member');
     guildMember.roles.add(welcomeRole);
 });
-
-
-const badjokes = 
-[
-    "Why did the chicken cross the road? Because it was disabled",
-    "I want to fucking kill myself.. That's funny right?",
-    "IS THAT A SUPRA?",
-    "Is that a transformer?",
-    "WHAT DO YOU MEANNNN THO?",
-    "Stop. Get some help.",
-    "Error. System reboot. Sike, fuck you.",
-    "What do you call an alligator that wants to be a detective? An investigator. ACK ACK ACK",
-    "HAMBURGER PLS",
-];
