c0970c8db2023f3d9524d49e5a7e3920a956f7c1
NIASKA <mkman751@yahoo.com>
2021-07-05 08:24:42 -0500

    test

--- commands/help.js
+++ commands/help.js
@@ -4,10 +4,8 @@ module.exports = {
     name: 'help',
     description: 'List of commands available',
     async execute(message, args, cmd, client, Discord, profileData) {
-        const Embed = new MessageEmbed()
-        .setTitle("Commands")
-        .setColor("RED")
-        .setDescription(`
+        let list1 =(`
+        Basic Commands: \n
         !ping \n
         !play \n
         !stop \n
@@ -19,9 +17,10 @@ module.exports = {
         !jokes \n
         !roast \n
         !wey \n
-        !poll \n
-        \n
-        Economy commands: \n
+        !poll \n`)
+
+        let list2 =(`
+        Economy Commands: \n
         !balance \n
         !daily \n
         !beg \n
@@ -34,8 +33,50 @@ module.exports = {
         !rob \n
         !hunt \n
         !search \n
-        !pay \n`);
+        !pay \n
+        `)
+
+        list3 =(`
+        RPG Commands \n
+        Under Development
+        `)
+
+        let pages = [list1, list2, list3]
+        let page = 1
+
+        const Embed = new MessageEmbed()
+        .setColor("RED")
+        .setFooter(`Page: ${page} / ${pages.length}`)
+        .setDescription(pages[page - 1])
+        let msg = await message.author.send(Embed)
+
+        if(pages.length === 1) return;
+
+        await msg.react("➡️");
+        await msg.react("⬅️");
+
+        const rightFilter = (reaction, user) => reaction.emoji.name === "➡️" && user.id === message.author.id;
+        const leftFilter = (reaction, user) => reaction.emoji.name === "⬅️" && user.id === message.author.id;
         
-        message.author.send(Embed);
+        const right = msg.createReactionCollector(rightFilter, {time: 900000, dispose: true});
+        const left = msg.createReactionCollector(leftFilter, {time: 900000, dispose: true});
+
+        left.on("collect", erase => {
+            erase.users.remove(message.author.id);
+            if(page === 1) return;
+            page--;
+            Embed
+            .setDescription(pages[page-1])
+            .setFooter(`Page: ${page} / ${pages.length}`);
+            msg.edit(Embed)
+        })
+
+        right.on("collect", erase => {
+            erase.users.remove(message.author.id);
+            if(page === pages.length) return;
+            page++;
+            Embed.setDescription(pages[page-1]).setFooter(`Page: ${page} / ${pages.length}`);
+            msg.edit(Embed)
+        })
     }
 };
