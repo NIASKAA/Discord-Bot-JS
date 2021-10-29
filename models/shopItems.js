b8f3b2e77179bb5beb3c1309d1260a4de6e6288d
NIASKA <mkman751@yahoo.com>
2021-10-29 13:35:37 -0500

    doing maintenence

--- models/shopItems.js
+++ models/shopItems.js
@@ -39,49 +39,4 @@ module.exports = [
         price: 65000,
         image: 'https://imgur.com/my8lV9x.png'
     },
-    {
-        name: 'black neos helmet',
-        price: 1500,
-        image: "https://imgur.com/LGT1wjM.png"
-    },
-    {
-        name: 'black neos armor',
-        price: 1500,
-        image: "https://imgur.com/b7OjuY6.png"
-    },
-    {
-        name: 'black neos legs',
-        price: 1500,
-        image: "https://imgur.com/W5CW5Kx.png"
-    },
-    {
-        name: 'chaos hat',
-        price: 1500,
-        image: "https://imgur.com/JWmXcvn.png"
-    },
-    {
-        name: 'black chaos robe',
-        price: 1500,
-        image: "https://imgur.com/Vqwp2RI.png"
-    },
-    {
-        name: 'black chaos leggings',
-        price: 1500,
-        image: "https://imgur.com/XQI6fob.png"
-    },
-    {
-        name: 'dark identity',
-        price: 1500,
-        image: "https://imgur.com/Vi78RQM.png"
-    },
-    {
-        name: 'dark katinas',
-        price: 1500,
-        image: "https://imgur.com/Vvc9dOs.png"
-    },
-    {
-        name: 'dark studded pants',
-        price: 1500,
-        image: "https://imgur.com/sYS24At.png"
-    }
 ];
